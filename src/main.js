7a6508865413dbff53aa6f623c1c3673e6249976
sobird <i@sobird.me>
2024-01-31 11:15:37 +0800

    javascript-action

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
-
+const { wait } = require('./wait')
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
@@ -10,6 +10,17 @@ async function run() {
     // The `who-to-greet` input is defined in action metadata file
     const whoToGreet = core.getInput('who-to-greet', { required: true })
     core.info(`Hello, ${whoToGreet}!`)
+
+    // The `milliseconds` input is defined in action metadata file
+    const ms = core.getInput('milliseconds', { required: true })
+
+    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+    core.debug(`Waiting ${ms} milliseconds ...`)
+
+    // Log the current timestamp, wait, then log the new timestamp
+    core.debug(new Date().toTimeString())
+    await wait(parseInt(ms, 10))
+    core.debug(new Date().toTimeString())
 
     // Get the current time and set as an output
     const time = new Date().toTimeString()
